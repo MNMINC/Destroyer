7282216d56082b9abcc707b59a47e0648a480329
MNMINC <157410433+MNMINC@users.noreply.github.com>
2024-02-26 21:08:09 +0000

    Rat Trap

--- exterpreter.cpp
+++ exterpreter.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 using namespace std;
 
 int main()
@@ -11,23 +12,39 @@ int main()
     exit(0);
   }
     
+  bool c1 = true;
+  bool c2 = true;
   for (int i = 0; i < 100; ++i) 
   {
-    ofs << i;
+    ofs << i << ":";
+    bool e1 = false;
+    bool e2 = false;
     for (int s1 = 0; s1 < i; ++s1)
     {    
       for (int s2 = 0; s2 < i; ++s2)
       {      
         for (int s3 = 0; s3 < i; ++s3)
         {
-          if (s1 * s1 * s1 + s2 * s2 * s2 + s3 * s3 * s3 == i * i * i)
+          if (pow(s1, 3) + pow(s2, 3) + pow(s3, 3) == pow(i, 3))
           {
-            ofs << s1 << s2 << s3;
+            ofs << s1 << "." << s2 << "." << s3 << "|";
+            e1 = true;
+          }
+          if (IsPrime(s1) && IsPrime(s2) && IsPrime(s3) && s1 + s2 + s3 == i)
+          {
+            ofs << s1 << "-" << s2 << "-" << s3 << "|";
+            e2 = true;
           }
         }
       }
     }
+    ofs << e1 << e2 << "/";
+    if (!e1) 
+      c1 = false;
+    if (!e2) 
+      c2 = false;
   }
+  ofs << c1 << c2;
 }
 
 bool IsPrime(int n) 
